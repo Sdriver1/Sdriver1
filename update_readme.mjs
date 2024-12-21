b6c4af694e896aa9382545dc4189012031dc6853
Sdriver1 <sjdriver08@gmail.com>
2024-12-20 22:02:25 -0500

    format

--- update_readme.mjs
+++ update_readme.mjs
@@ -247,11 +247,11 @@ module.exports = {
         {
           name: \`Project\`,
           value: \`- Center.chat - *coming soon*
-                   - DevEco - discord.gg/deveco\`,
+                  - DevEco - discord.gg/deveco\`,
         },
         {
           name: \`Websites\`,
-          value: \`//Pridebot Websites
+          value: \`// Pridebot Websites
                   - https://pridebot.xyz - Website for Pridebot
                   - https://pfp.pridebot.xyz - Pridebot PFP API
 
