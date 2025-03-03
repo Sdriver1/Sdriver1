344ea78e7035b07787d6718721d72490d9c74b8a
Sdriver1 <sjdriver08@gmail.com>
2025-03-03 11:38:16 -0500

    updating age

--- update_readme.mjs
+++ update_readme.mjs
@@ -180,6 +180,22 @@ async function getBotStats() {
   return stats;
 }
 
+function calculateAge(birthday) {
+  const today = new Date();
+  const birthDate = new Date(birthday);
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const monthDifference = today.getMonth() - birthDate.getMonth();
+
+  if (
+    monthDifference < 0 ||
+    (monthDifference === 0 && today.getDate() < birthDate.getDate())
+  ) {
+    age--;
+  }
+
+  return age;
+}
+
 async function getYANGstats() {
   let stats = { visits: 0, clicks: 0 };
 
